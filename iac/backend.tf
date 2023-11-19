f9e6d7578b8a0e23950fe99ef8a157eab9795d3c
“safo” <lekanaws@gmail.com>
2023-11-18 18:24:31 -0600

    error message

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "cicd-rentzone-state-bucket"
+    bucket         = "cicd-rentzone-bucket"
     key            = "cicd-rentzone/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "cicd-state-lock"
